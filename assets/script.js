d4de526c84072319b9784fedceae38dfa7df05d2
JulianaAmoasei <late.parrot@gmail.com>
2018-07-03 00:27:14 -0300

    adicionado menu dinamico

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,32 @@
 var dropMenu = document.getElementById("drop-menu");
 dropMenu.addEventListener("change", carregaPrograms);
 
+window.onload = carregaMenu();
+
+function carregaMenu(){
+  var dropMenu = document.getElementById("drop-menu");
+  dropMenu.innerHTML = "";
+  var none = document.createElement("option");
+  none.value = "none";
+  none.innerHTML = "selecione";
+  dropMenu.appendChild(none);
+  for(sede in data){
+    var itemMenu = document.createElement("option");
+    itemMenu.value = sede;
+    itemMenu.innerHTML = sede;
+    dropMenu.appendChild(itemMenu);
+  }
+};
+
 function carregaPrograms(){
-  var option = dropMenu.value;
+  var sede = dropMenu.value;
   var exibePrograms = document.getElementById("exibePrograms");
   exibePrograms.innerHTML = "";
-  for(batata in data[option]){
-    for(xuxu in data[option][batata]["students"]){
-      (data[option][batata]["students"][xuxu]["photo"]);
-      img = document.createElement("img");
-      img.src = data[option][batata]["students"][xuxu]["photo"];
+  for(turma in data[sede]){
+    for(i in data[sede][turma]["students"]){
+      (data[sede][turma]["students"][i]["photo"]);
+      var img = document.createElement("img");
+      img.src = data[sede][turma]["students"][i]["photo"];
       exibePrograms.appendChild(img);
     }
   }
